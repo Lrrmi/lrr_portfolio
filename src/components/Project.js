9911c4c16fd379f909a0a6fc1915d68c98161ede
AliA12336 <amadams12336@gmail.com>
2026-01-06 22:26:48 -0500

    Update arrows

--- src/components/Project.js
+++ src/components/Project.js
@@ -48,12 +48,12 @@ export default function Project({ projectTitle }) {
         <div className="project" ref={scrollRef}>
             <div className="arrows">
                 <Link className="leftArrow" onClick={() => {scrollRef.current?.scrollTo({left: 0, behavior: "smooth"})}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#dfdedf" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#dfdedf" class="bi bi-arrow-left" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
                     </svg>
                 </Link>
                 <Link className="rightArrow" onClick={() => {scrollRef.current?.scrollTo({left: scrollRef.current.scrollWidth, behavior: "smooth"})}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#dfdedf" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#dfdedf" class="bi bi-arrow-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
                     </svg>
                 </Link>
