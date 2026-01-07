8b7b5e8389dd4b88f7b4114357579267ac3cdf6f
AliA12336 <amadams12336@gmail.com>
2026-01-06 22:43:06 -0500

    Update arrows

--- src/components/Potpourri.js
+++ src/components/Potpourri.js
@@ -5,12 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Potpourri() {
     return (
         <div className="arrowWrapper">
-        <div className="potpourri">
-            <div className="drawLine"></div>
-            {potpourri.map(newImage => (
-                <img draggable="false" className="potpourriImage" src={newImage} alt="Visual of a project" />
-            ))}
-            <div className="arrows">
+             <div className="arrows">
             <Link className="topArrow" onClick={() => window.scrollBy({top: -window.innerHeight, behavior: "smooth"})}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#dfdedf" class="bi bi-arrow-up" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
             </svg>
@@ -21,6 +16,12 @@ export default function Potpourri() {
                 </svg>
             </Link>
             </div>
+        <div className="potpourri">
+            <div className="drawLine"></div>
+            {potpourri.map(newImage => (
+                <img draggable="false" className="potpourriImage" src={newImage} alt="Visual of a project" />
+            ))}
+           
         </div>
         </div>
     );
