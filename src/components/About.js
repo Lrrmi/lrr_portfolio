3314ac37408687f2f2ccbc4f223b9e909c484070
aadams <amadams12336@gmail.com>
2026-01-07 19:11:21 -0500

    Replace placeholder text

--- src/components/About.js
+++ src/components/About.js
@@ -8,48 +8,21 @@ export default function About() {
                 <div className="aboutContent">
                     <h2 className="aboutHeader">Design Philosophy</h2>
                     <p>
-                        Mr. Dursley was the director of a firm called Grunnings, which made drills.
-                        He was a big, beefy man with hardly any neck, although he did have a very
-                        large mustache. Mrs. Dursley was thin and blonde and had nearly twice the
-                        usual amount of neck, which came in very useful as she spent so much of her
-                        time craning over garden fences, spying on the neighbors. The Dursleys had a
-                        small son called Dudley and in their opinion there was no finer boy anywhere.
+                        <b>About</b>
                         <br></br><br></br>
-                        The Dursleys had everything they wanted, but they also had a secret, and
-                        their greatest fear was that somebody would discover it. They didn’t think
-                        they could bear it if anyone found out about the Potters. Mrs. Potter was Mrs.
-                        Dursley’s sister, but they hadn’t met for several years; in fact, Mrs. Dursley
-                        pretended she didn’t have a sister, because her sister and her good-for-nothing
-                        husband were as unDursleyish as it was possible to be. The Dursleys
-                        shuddered to think what the neighbors would say if the Potters arrived in the
-                        street. The Dursleys knew that the Potters had a small son, too, but they had
-                        never even seen him. This boy was another good reason for keeping the
-                        Potters away; they didn’t want Dudley mixing with a child like that.
-                        When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story
-                        starts, there was nothing about the cloudy sky outside to suggest that strange
-                        and mysterious things would soon be happening all over the country. Mr.
-                        Dursley hummed as he picked out his most boring tie for work, and Mrs.
-                        Dursley gossiped away happily as she wrestled a screaming Dudley into his
-                        high chair.
+                        I am an American industrial designer trained in both traditional woodworking techniques and contemporary manufacturing methods of design. With a Masters of Fine Art in Furniture Design from Savannah College of Art and Design, I am committed to progress through iteration, always seeking with each new project to produce a better product than the last.
                         <br></br><br></br>
-                        None of them noticed a large, tawny owl flutter past the window.
-                        At half past eight, Mr. Dursley picked up his briefcase, pecked Mrs.
-                        Dursley on the cheek, and tried to kiss Dudley good-bye but missed, because
-                        Dudley was now having a tantrum and throwing his cereal at the walls. “Little
-                        tyke,” chortled Mr. Dursley as he left the house. He got into his car and
-                        backed out of number four’s drive.
+                        With a keen interest in how design’s role affects society and our physical world, I am committed to the process of improvement so that my craft may continue to evolve in an ever-changing world. My desire for refinement and knowledge is never ceasing, and is expressed through continued research into new materials, manufacturing methods, following design trends, and new tools of trade.
                         <br></br><br></br>
-                        It was on the corner of the street that he noticed the first sign of something
-                        peculiar — a cat reading a map. For a second, Mr. Dursley didn’t realize what
-                        he had seen — then he jerked his head around to look again. There was a
-                        tabby cat standing on the corner of Privet Drive, but there wasn’t a map in
-                        sight. What could he have been thinking of? It must have been a trick of the
-                        light. Mr. Dursley blinked and stared at the cat. It stared back. As Mr. Dursley
-                        drove around the corner and up the road, he watched the cat in his mirror. It
-                        was now reading the sign that said Privet Drive — no, looking at the sign;
-                        cats couldn’t read maps or signs. Mr. Dursley gave himself a little shake and
-                        put the cat out of his mind. As he drove toward town he thought of nothing
-                        except a large order of drills he was hoping to get that day.
+                        Having served both as a design consultant assisting others and as a leading design director of projects, I understand the importance of working within branding constraints to create products that delight. If the client requires something completely new, I have created innovative products as well. In this regard, I can flexibly accommodate and fill whichever role is needed for the design process at the time.
+                        <br></br><br></br>
+                        I believe in blending our human needs with an appreciation for natural beauty. We should not rely on mimicry to imitate other materials, but should strive to bring out the unique beauty of each material through its intrinsic qualities, aesthetic values, and even individual imperfections. My goal is not only to create durable, delightful and desirable products for the betterment of our environment along with the people it serves, but also be sure to have fun along the way and never take things too seriously.
+                        <br></br><br></br><br></br>
+                        <b>Design Process</b>
+                        <br></br><br></br>
+                        Initially I work closely with the client to translate their vision into drawings and interactive 3-D models in order to materialize their idea into a defined product. While working with clients or other designers, I believe in collaborative iteration, focusing the design process on a successful outcome. All the participants have initial ideas about a project and they are like different colors split from a prism. The goal is to be sure we are on the same wavelength to be sure the design path is properly illuminated.
+                        <br></br><br></br>
+                        Once the design constraints are defined, I work within these parameters to produce renders, scale models, physical prototyping, or whatever is needed for the client to see and approve the production work. Then I work with the client’s team or help select contracted vendors to cost-effectively produce the desired item.
                     </p>
                 </div>
                 <br></br><br></br>
