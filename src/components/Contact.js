ae0449af6aa2d6654036c1a2d6f7225f5b430d97
Lrrmi <rahl.adams1@gmail.com>
2026-01-11 11:18:38 -0600

    m

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,32 +7,29 @@ export default function Contacts() {
                 <h2 className="contactsHeader">Contact</h2>
                 <br></br>
                 <p className="contactsDetails">
-                    Mr. Dursley was the director of a firm called Grunnings, which made drills.
-                    He was a big, beefy man with hardly any neck, although he did have a very
-                    large mustache. Mrs. Dursley was thin and blonde and had nearly twice the
-                    usual amount of neck, which came in very useful as she spent so much of her
-                    time craning over garden fences, spying on the neighbors. The Dursleys had a
-                    small son called Dudley and in their opinion there was no finer boy anywhere.
+                    For inquire about new projects or design services please email me at:
                 </p>
                 <br></br>
                 <div className="contactsLinkedin">
                     <p className="contactsLinkedinLabel">LinkedIn:</p>
-                    <a className="contactsLinkedinLink" href="https://linkedin.com" target="_blank" rel="noopener noreferrer">https://linkedin.com</a>
+                    <a className="contactsLinkedinLink" href="https://linkedin.com" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/robertleeadams</a>
                 </div>
                 <br></br>
                 <div className="contactsEmail">
                     <p className="contactsEmailLabel">Email:</p>
-                    <p className="contactsEmail">prinny@hotmail.com</p>
+                    <p className="contactsEmail">rmladams25@gmail.com</p>
                 </div>
                 <br></br>
                 <h2 className="contactsServicesHeader">Services</h2>
                 <br></br>
                 <div className="servicesContent">
-                    <p>Offers Full Design Service</p>
+                    <p>Product & Furniture Design Services</p>
                     <br></br>
-                    <p>Concept Modeling and Initial Manufacturing Drawings</p>
+                    <p>3D Concept/Render Modeling & Production Modeling</p>
                     <br></br>
-                    <p>Small Scale Prototyping</p>
+                    <p>Rendering & Drafting</p>
+                    <br></br>
+                    <p>Small & Full Scale Prototyping services</p>
                     <br></br>
                     <div className="drawLine"></div>
                     <br></br>
